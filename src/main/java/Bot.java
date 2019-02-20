cc1317a850d32462983781ef67661a20b6427a1b
Will Thompson <wbthompson14@gmail.com>
2019-02-19 23:52:48 -0800

    Basic bot ping/pong functionality

---
+++ src/main/java/Bot.java
@@ -0,0 +1,28 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import utility.ConfigReader;
+
+public class Bot {
+
+    private static final Logger log = LogManager.getFormatterLogger();
+
+    private void start() {
+        log.info("Starting MusikDrone");
+
+        DiscordApi api = new DiscordApiBuilder().setToken(ConfigReader.readProperties("token")).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Bot me = new Bot();
+        me.start();
+    }
+}
