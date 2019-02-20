84fa899f75708c9871ae84a697357c4f95ad6847
Will Thompson <wbthompson14@gmail.com>
2019-02-19 23:52:48 -0800

    Basic bot ping/pong functionality

---
+++ src/main/java/utility/ConfigReader.java
@@ -0,0 +1,16 @@
+package utility;
+
+import com.typesafe.config.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ConfigReader {
+
+    private static final Logger logger = LogManager.getFormatterLogger();
+
+    public static String readProperties(String key) {
+        String value = ConfigFactory.load().getString(key);
+        logger.info("Reading in configuration property: [%s] = %s", key, value);
+        return value;
+    }
+}
