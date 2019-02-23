fb8495240e751eef78793185a3eec3dd30eafafd
Will Thompson <wbthompson14@gmail.com>
2019-02-23 00:43:56 -0800

    Switched discord API wrapper to use JDA instead of Javacord

---
+++ src/main/java/com/github/mealsquad/listeners/PingListener.java
@@ -0,0 +1,15 @@
+package com.github.mealsquad.listeners;
+
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class PingListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().equals("!ping")) {
+            event.getTextChannel().sendMessage("Pong!").queue();
+        }
+    }
+}
